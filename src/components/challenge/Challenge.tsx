995815fed9d0495cff56f4ef0473179cb9895e88
Manoel Duran <manoel.duran@hotmail.com>
2023-04-22 19:38:48 -0300

    challengeForm with re-render bug

--- src/components/challenge/Challenge.tsx
+++ src/components/challenge/Challenge.tsx
@@ -1,13 +1,63 @@
+import{ useCallback, useState } from "react";
+import { toast } from 'react-toastify';
 import styles from "./Challenge.module.css";
+import { canCreateChallenge } from "./validations";
 
-export default function Challenge({ userName }: { userName: string }) {
+interface FormData {
+  token: string;
+  email: string;
+}
+interface ChallengeProps {
+  getUser: (user: any) => void
+}
+export default function  Challenge({getUser}: ChallengeProps) {
+  const [token, setToken] = useState('')
+  const [email, setEmail] = useState('')
+  const onSubmit = async () => {
+    try {
+     const parsedData = {
+       token,
+       email
+     } as FormData;
+     console.log('parsedData', parsedData)
+   await canCreateChallenge(parsedData)
+   //const user =  api.asdsadsadsadsa
+     //getUser(user)
+     console.log('parsedData', parsedData)
+     toast.success('Hello world!',{
+       position: "top-right",
+       autoClose: 5000,
+       hideProgressBar: false,
+       closeOnClick: true,
+       pauseOnHover: true,
+       draggable: true,
+       progress: undefined,
+       theme: "light",
+       });
+
+    } catch(err) {
+    
+     toast.error('Error',{
+       position: "top-right",
+       autoClose: 5000,
+       hideProgressBar: false,
+       closeOnClick: true,
+       pauseOnHover: true,
+       draggable: true,
+       progress: undefined,
+       theme: "light",
+       });
+    }
+ }
   return (
     <div className={styles.challengeContainer}>
-      <h3>Challenge {userName}</h3>
-
-      <a href="https://lichess.org/u9GSkAnF" target="_blank">
-        <button>CHALLENGE</button>
-      </a>
+      <form  onSubmit={onSubmit}>
+        <div className={styles.challengeFormContainer}>
+        <input title="token" type="text" onChange={(e) =>  setToken(e.target.value)} placeholder="Enter your lichess token"/>
+        <input title="email" type="text" onChange={(e) =>  setEmail(e.target.value)} placeholder="Enter your lichess email account"/>
+        </div>
+        <button type="submit">CHALLENGE ME!</button>
+      </form>
     </div>
   );
 }
